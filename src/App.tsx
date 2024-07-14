573e6251560eb0b72e1e9e2ca1b6aeb2277180e7
Marcin Tutucki <m.tutucki@gmail.com>
2024-07-14 20:14:43 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import mobileBackground from './assets/background/bg-main-mobile.png'
+
+function App() {
+  return (
+    <>
+      <div className='min-h-[30vh]'>elo</div>
+    </>
+  );
+}
+
+export default App;
